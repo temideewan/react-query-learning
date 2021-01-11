4db80e35b7aa6f03469e70f2bd9d9756336e87f8
temidayo omoyajowo <temideewan@gmail.com>
2021-01-11 19:42:46 +0100

    fix: pagination in pages page

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,36 +1,65 @@
-import React from 'react'
-import {useQuery} from 'react-query';
-import Planet from './Planet'
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import Planet from "./Planet";
 
-
-const fetchPlanets = async(greeting) => {
-    console.log(greeting.queryKey[1]);
-    const res = await fetch('https://www.swapi.tech/api/planets');
-    return res.json();
-}
-
+const fetchPlanets = async ({ queryKey }) => {
+  const page = queryKey[1];
+  const res = await fetch(
+    `https://www.swapi.tech/api/planets?page=1&limit=60`
+  );
+  return res.json();
+};
 
 const Planets = () => {
-    const {data, status} = useQuery(['planets', 123], fetchPlanets, {staleTime: 2000,cacheTime:10, onSuccess:()=> console.log('data has been fetched successfully bruh')});
-    console.log(data);
-    return ( 
+  const [page, setPage] = useState(1);
+  const { data, status } = useQuery(["planets", 1], fetchPlanets, {
+    staleTime: 2000,
+    cacheTime: 10,
+    keepPreviousData: true,
+    onSuccess: () => console.log("data has been fetched successfully bruh"),
+  });
+  console.log(data);
+  
+  return (
+    <div>
+      
+      <h2>Planets</h2>
+      {/* <p>{status}</p> */}
+      <button onClick={() => setPage((old) => Math.max(old - 1, 1))}
+      disabled={page === 1}
+      >
+        prev page
+      </button>
+      {page}
+      <button onClick={() => {
+        const lastElementUid = data.results[data.results.length-1].uid;
+        const lastElementOfEachPage = page* 10;
+        const uidOfLastPaginatedPage = data.results[lastElementOfEachPage -1].uid;
+        const check = uidOfLastPaginatedPage === lastElementUid;
+        return setPage((old) => check? old: old+1)}}
+      disabled={data.results[page* 10 -1].uid === data.results[data.results.length-1].uid }>
+        next page
+      </button>
+      {status === "loading" && <div>Loading data...</div>}
+      {status === "error" && <div>Error fetching data</div>}
+      {status === "success" && (
         <div>
-            <h2>Planets</h2>
-        {/* <p>{status}</p> */}
-        {status === 'loading' && (
-            <div>Loading data...</div>
-        )}
-        {status === 'error' && (
-            <div>Error fetching data</div>
-        )}
-        {status === 'success' && (
-            <div>
-                
-                {data.results.map(planet => <Planet key={planet.name} planet={{name:planet.name, url:planet.url}}/>)}
-            </div>
-        )}
+          {data.results
+            .filter((planet, index) => {
+              const upperLimit = page * 10;
+              const lowerLimit = 10 * (page - 1);
+              return index >= lowerLimit && index < upperLimit;
+            })
+            .map((planet, index) => (
+              <Planet
+                key={planet.name}
+                planet={{ name: planet.name, url: planet.url }}
+              />
+            ))}
         </div>
-     );
-}
- 
+      )}
+    </div>
+  );
+};
+
 export default Planets;
